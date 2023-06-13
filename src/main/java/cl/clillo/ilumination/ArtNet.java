a55d09844b17ece87ba2d0f64102288c8212aff3
carlos-lillo_globant <carlos.lillo@globant.com>
2023-06-13 16:31:58 -0400

    dsfsd

--- src/main/java/cl/clillo/ilumination/ArtNet.java
+++ src/main/java/cl/clillo/ilumination/ArtNet.java
@@ -4,12 +4,44 @@ import ch.bildspur.artnet.ArtNetClient;
 
 public class ArtNet {
 
-    public static void main(String[] args) {
-        ArtNetClient artNetClient = new ArtNetClient();
+    private final ArtNetClient artNetClient;
+    private final byte[] dmxData;
+
+    private static final class InstanceHolder {
+        private static final ArtNet instance = new ArtNet();
+    }
+
+    public static ArtNet getInstance() {
+        return ArtNet.InstanceHolder.instance;
+    }
+
+    private ArtNet(){
+        artNetClient = new ArtNetClient();
+        dmxData = new byte[512];
+        for (int i=0; i<512; i++)
+            dmxData[i] = 0;
+
         artNetClient.start();
-        byte[] dmxData = new byte[512];
-        dmxData[0] = (byte) 128;
-        artNetClient.broadcastDmx(0,1, dmxData);
-        //artNetClient.stop();
+    }
+
+    public void send(final int channel, final int value){
+        send(channel, (byte) value);
+    }
+
+    public void send(final int channel, final byte value){
+        dmxData[channel] = (byte)value;
+        artNetClient.broadcastDmx(0, 0, dmxData);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ArtNet artNet = ArtNet.getInstance();
+        artNet.send(419, 255);
+        for (int i=0; i<10000; i++) {
+            artNet.send(423, 255);
+            Thread.sleep(600);
+
+            artNet.send(423, 0);
+            Thread.sleep(600);
+        }
     }
 }
