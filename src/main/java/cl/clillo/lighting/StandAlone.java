374dc0f25436c0c7536bb3030f3843afb879556f
carlos-lillo_globant <carlos.lillo@globant.com>
2024-01-05 10:17:58 -0300

    add virtualdj events integration

--- src/main/java/cl/clillo/lighting/StandAlone.java
+++ src/main/java/cl/clillo/lighting/StandAlone.java
@@ -11,6 +11,10 @@ import cl.clillo.lighting.utils.CSVSupport;
 import cl.clillo.lighting.utils.FileUtils;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 
 public class StandAlone {
 
@@ -24,8 +28,12 @@ public class StandAlone {
            }
     }
 
-    public static void main(String[] args) {
-   //     StandAlone standAlone = new StandAlone();
+    public static void main(String[] args) throws IOException {
+       // Socket socket = new Socket();
+       // socket.connect(new InetSocketAddress("google.com", 80));
+       // System.out.println(socket.getLocalAddress());
+
+        //     StandAlone standAlone = new StandAlone();
       //  CSVSupport csvSupport = new CSVSupport();
      //   csvSupport.printFunctionIds();
       // csvSupport.rewriteFunctions();
@@ -34,10 +42,9 @@ public class StandAlone {
 
         Logger root = context.getLogger(Logger.ROOT_LOGGER_NAME);
         root.setLevel(Level.INFO);
-
         ArtNet.setMode(ArtNet.ArtNetMode.DIRECT_ART_NET);
      //   ArtNet.setMode(ArtNet.ArtNetMode.HTTP_ART_NET);
-      // ArtNet.setMode(ArtNet.ArtNetMode.NON_ART_NET);
+     //  ArtNet.setMode(ArtNet.ArtNetMode.NON_ART_NET);
 
        final ControllerJFrame controllerJFrame = new ControllerJFrame();
         controllerJFrame.start();
